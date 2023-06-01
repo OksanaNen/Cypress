ea76665feb6b046161375935352335d99ee84e8f
Oksana <deminaon@gmail.com>
2023-06-01 11:58:40 +1000

    Домашнее задание 15.1

---
+++ cypress/e2e/loadingAnElement.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe( 'The element on the page loaded', () => {
+  beforeEach(() => {
+    cy.visit('https://secret-santa.ru/')
+  })
+   
+  it('The title h3', () => {
+    cy.get('h3.title')
+      .should('be.visible')
+  })
+})
