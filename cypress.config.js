0f27cb5f3963fb39ad211c814879a244faa4f3af
Oksana <deminaon@gmail.com>
2023-06-03 14:30:08 +1000

    Домашнее задание 15.2

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "as4p1t",
   e2e: {
+    baseUrl: 'https://santa-secret.ru',
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/checkingLlinks.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+// Добавьте второй спек с тестами: проверяем ссылки на странице пользователя после логина
+
+// - коробки
+// - создать коробку
+// - быстрая жеребьевка
+// - личный кабинет
+
+// У вас должно быть 4 теста с проверками элементов на странице после перехода по ссылке, а также проверкой URL.
+
+// Обязательно используйте baseUrl в конфигурации (стейджинговое окружение).
+
+describe('Checking links on the user page after login', () => {
+  const baseUrl = Cypress.config('baseUrl');
+
+  beforeEach('Login', () => {
+    cy.visit('/login');
+    cy.get(':nth-child(3) > .frm').type('deminaon@gmail.com{enter}');
+    cy.get(':nth-child(4) > .frm').type('qwerty123{enter}');
+    cy.get('.btn-main').click();
+  });
+  
+  it('Сheck link - Boxes', () => {
+    cy.get('.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"]')
+      .click();
+    cy.url().should('be.equal',`${baseUrl}/account/boxes`); 
+    cy.get('.toggle-menu-item--active > .txt--med').should('be.visible');  
+  });
+
+  it('Check link - Create box', () => {
+    cy.get('.home-page-buttons > [href="/box/new"] > .btn-main').click();
+    cy.url().should('be.equal',`${baseUrl}/box/new`);
+    cy.get('.form-card__header > .txt-h3--semi').should('be.visible');
+  });
+
+  it('Check link - Quick draw', () => {
+    cy.get('[href="/randomizer"] > .btn-secondary').click();
+    cy.url().should('be.equal',`${baseUrl}/randomizer`);
+    cy.get('.tip').should('be.visible');
+  });
+  
+  it('Check link - Account', () => {
+    cy.get('.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account"]').click();
+    cy.url().should('be.equal',`${baseUrl}/account`);
+    cy.get('#account_profile_settings').should('be.visible');
+  });  
+})
